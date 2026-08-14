61d87fb9af18ce1f719243f8df04b81603184b18
Konstantin S. <havendv@gmail.com>
2026-08-14 19:40:18 +0400

    feat: Updated OpenAPI spec (#62)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Resend/Generated/Resend.SegmentsClient.g.cs
+++ src/libs/Resend/Generated/Resend.SegmentsClient.g.cs
@@ -38,10 +38,17 @@ namespace Resend
 
         /// <inheritdoc/>
         public global::System.Func<string> CreateIdempotencyKey { get; set; } = () => global::System.Guid.NewGuid().ToString("D");
+
+        internal global::System.Lazy<global::System.Text.Json.Serialization.JsonSerializerContext> JsonSerializerContextProvider { get; set; } = new(() => global::Resend.SourceGenerationContext.Default);
+
         /// <summary>
         /// 
         /// </summary>
-        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext { get; set; } = global::Resend.SourceGenerationContext.Default;
+        public global::System.Text.Json.Serialization.JsonSerializerContext JsonSerializerContext
+        {
+            get => JsonSerializerContextProvider.Value;
+            set => JsonSerializerContextProvider = new(() => value);
+        }
 
 
         /// <summary>
